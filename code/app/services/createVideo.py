8bc65c8cdabe2bafb2b2edee50c21634d8f6c593
mohammed <I443892>
2023-04-04 07:57:20 +0200

    try to add audio example (not  working yet)

--- code/app/services/createVideo.py
+++ code/app/services/createVideo.py
@@ -8,7 +8,10 @@ import os
 def create_video_from_text(transcript: str):
 
     print(transcript)
-    with open('temp_files/transcript.txt', 'w') as text_file:
+    directory = 'temporary'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with open('temporary/transcript.txt', 'w') as text_file:
         # Write the text to the file
         text_file.write(transcript)
 
@@ -28,14 +31,14 @@ def get_phonemes():
     url = 'http://localhost:8765/transcriptions?async=false'
 
     # Set the files and data for the HTTP POST request
-    files = {'audio': ('example.mp3', open('/path/to/temp/folder/example.mp3', 'rb')),
-             'transcript': ('example.txt', open('/path/to/temp/folder/example.txt', 'rb'))}
+    files = {'audio': ('example.mp3', open('temporary/example.mp3', 'rb')),
+             'transcript': ('example.txt', open('temporary/example.txt', 'rb'))}
 
     # Send the HTTP POST request to the external API
     response = requests.post(url, files=files)
 
     # Save the response to a file
-    with open('phonemes.txt', 'w') as f:
+    with open('temporary/phonemes.txt', 'rw') as f:
         f.write(response.text)
 
     # Return the response from the external API
@@ -46,11 +49,16 @@ def convert_text_to_speach(transcript: str):
 
     # this is an example of audio file later we will use Amazon poly TODO
     # load the audio file using the path
-    audio_file = AudioSegment.from_file('./temp_files/example.mp3', format='mp3')
+    audio_file = AudioSegment.from_file('temporary/example.mp3', format='mp3')
 
 
     # after geting the audio from Amason poly we save it temporarly
-    audio_file.export('../temp_files/example.mp3', format='mp3')
+    audio_file.export('temporary/example.mp3', format='mp3')
 
     # Get the audio data //No need maybe future use
     # return audio_file.raw_data
+
+
+def lazykh(transcript: str,audio:AudioSegment,phonemes:str):
+# TODO
+    return 0
