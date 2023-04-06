cea84cd35965e0da187213c826b26c05807c8063
Mohammed Al-eryani <72073878+MohammedN-hash@users.noreply.github.com>
2023-04-06 12:55:39 +0200

    add remove tags for gentel for better phonemes format

--- code/app/controller/phonemesController.py
+++ code/app/controller/phonemesController.py
@@ -15,9 +15,38 @@ async def create_phonemes(request: Request, mp3Audio: UploadFile = File(...), te
     # execution time is 15.12s
     mp3Audio = await mp3Audio.read()
     textFile = await textFile.read()
-
-    # Do some processing with contents1 and contents2 to generate the text output
+    textFile = textFile.decode('utf-8')
+    textFile = removeTags(textFile)
+    # Send the audio and text to the gentel container
     phonemes = get_phonemes(mp3Audio, textFile)
 
     # Return the phonemes
     return {"phonemes": phonemes}
+
+
+import argparse
+import os.path
+
+
+def removeTags(script):
+  TO_REMOVE = ["[","]","/"]
+
+  newScript = script.replace("-"," ")
+  for charToRemove in TO_REMOVE:
+      newScript = newScript.replace(charToRemove,"")
+
+  while "<" in newScript:
+      start = newScript.index("<")
+      end = newScript.index(">")+1
+      newScript = newScript[:start]+newScript[end:]
+  while "  " in newScript:
+      newScript = newScript.replace("  "," ")
+  while "\n " in newScript:
+      newScript = newScript.replace("\n ","\n")
+  while " \n" in newScript:
+      newScript = newScript.replace(" \n","\n")
+  while newScript[0] == " ":
+      newScript = newScript[1:]
+
+  return newScript
+
